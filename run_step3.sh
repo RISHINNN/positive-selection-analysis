f6f8f4aeab4e90818a05c7b361fef9c9a6b84ae3
RISHINNN <137243814+RISHINNN@users.noreply.github.com>
2026-03-17 19:44:15 +0800

    Update run_step3.sh

--- run_step3.sh
+++ run_step3.sh
@@ -1,2 +1,1 @@
-cd /data01/lichen/05_project/08_Duozu/01_selection_press
-/data01/lichen/anaconda3/bin/python  run_paml_parallel_v4.py -t labeled_tree.nwk -p ./Codon_Alignments_Rebuild -m gene2species.txt -o ./PAML_Results -c 15
+python  run_paml_parallel.py -t labeled_tree.nwk -p ./Codon_Alignments_Rebuild -m mapping.txt -o ./PAML_Results -c 15
