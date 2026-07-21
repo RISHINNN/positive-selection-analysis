b4322a4c38882aca616ce0ccd8ffec8dfc9eb3bd
Chen Li <137243814+RISHINNN@users.noreply.github.com>
2026-07-21 20:16:08 +0800

    Switch to run_paml_parallel2.py for execution

--- run_step3.sh
+++ run_step3.sh
@@ -1,1 +1,2 @@
-python  run_paml_parallel.py -t labeled_tree.nwk -p ./Codon_Alignments_Rebuild -m mapping.txt -o ./PAML_Results -c 15
+#python  run_paml_parallel.py -t labeled_tree.nwk -p ./Codon_Alignments_Rebuild -m mapping.txt -o ./PAML_Results -c 15
+python  run_paml_parallel2.py -t labeled_tree.nwk -p ./Codon_Alignments_Rebuild -m mapping.txt -o ./PAML_Results -c 15
